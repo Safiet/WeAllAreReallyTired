4d3948b0cdf3f6b4a0647233cb7e50680c7617d0
Safiet <dsecarriers@gmail.com>
2021-03-11 19:29:18 -0600

    new file git added

---
+++ src/testTheGit/Git.java
@@ -0,0 +1,11 @@
+package testTheGit;
+
+public class Git {
+
+	public static void main(String[] args) {
+		System.out.println("this is git");
+		System.out.println("we are learning git");
+
+	}
+
+}
